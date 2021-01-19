1fc1fa40edebc3554cd1573685b4b0cc9d88c890
lbs1991 <zgzjlbs@163.com>
2021-01-19 17:02:50 +0800

    send mail with file is ok

--- sendmail2.py
+++ sendmail2.py
@@ -5,13 +5,21 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.header import Header
 
-sender = 'from@runoob.com'
-receivers = ['429240967@qq.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
 
+# 第三方 SMTP 服务
+mail_host = "smtp.qq.com"  # 设置服务器
+# mail_user = "zgzjlbs@163.com"  # 用户名
+mail_user = "122549797@qq.com"
+# mail_pass = "lbs1991wy"  # 口令
+mail_pass = "lwnqxrnuerncbgbc"  # 口令
+# sender = 'from@runoob.com'
+# receivers = ['429240967@qq.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
+receivers = ['zgzjlbs@163.com']
+sender  = '122549797@qq.com'
 # 创建一个带附件的实例
 message = MIMEMultipart()
-message['From'] = Header("菜鸟教程", 'utf-8')
-message['To'] = Header("测试", 'utf-8')
+message['From'] = Header("菜鸟教程")
+message['To'] = Header("测试")
 subject = 'Python SMTP 邮件测试'
 message['Subject'] = Header(subject, 'utf-8')
 
@@ -31,9 +39,15 @@ att2["Content-Type"] = 'application/octet-stream'
 att2["Content-Disposition"] = 'attachment; filename="runoob.txt"'
 message.attach(att2)
 
-try:
-    smtpObj = smtplib.SMTP('localhost')
+
+with smtplib.SMTP() as smtpObj:
+    smtpObj.connect(mail_host, 25)  # 25 为 SMTP 端口号
+    smtpObj.login(mail_user, mail_pass)
     smtpObj.sendmail(sender, receivers, message.as_string())
     print("邮件发送成功")
-except smtplib.SMTPException:
-    print("Error: 无法发送邮件")
+# try:
+#     smtpObj = smtplib.SMTP('localhost')
+#     smtpObj.sendmail(sender, receivers, message.as_string())
+#     print("邮件发送成功")
+# except smtplib.SMTPException:
+#     print("Error: 无法发送邮件")
